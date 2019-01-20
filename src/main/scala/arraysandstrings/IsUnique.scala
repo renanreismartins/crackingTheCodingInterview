f7a84ceff0841dc9c2ff4be88a54a6c8cc5a37a1
Renan Reis <renanreismartins@gmail.com>
2019-01-20 17:01:54 +0100

    Exe 1. Better implementation using HashMap

--- src/main/scala/arraysandstrings/IsUnique.scala
+++ src/main/scala/arraysandstrings/IsUnique.scala
@@ -1,11 +1,13 @@
 package arraysandstrings
 
+import scala.collection.immutable.HashMap
+
 object IsUnique {
 
   def isUnique(s: String): Boolean = {
     if (s.isEmpty) return true
     if (isUnique_(s.charAt(0), s.drop(1))) {
-      return isUnique(s.drop(1))
+      isUnique(s.drop(1))
     } else {
       false
     }
@@ -14,6 +16,18 @@ object IsUnique {
   private def isUnique_(c: Char, s: String): Boolean = {
     if (s.isEmpty) return true
     if (c.equals(s.charAt(0))) return false
-    return isUnique_(c, s.drop(1))
+    isUnique_(c, s.drop(1))
   }
+
+  // Implemented after checking the book solution
+  def isUniqueOptimized(s: String): Boolean = {
+    isUniqueOptimized_(s, HashMap.empty)
+  }
+
+  private def isUniqueOptimized_(s: String, table: HashMap[Char, Boolean]): Boolean = {
+    if (s.isEmpty) return true
+    if (table.get(s.charAt(0)).getOrElse(false)) return false
+    isUniqueOptimized_(s.drop(1), table + (s.charAt(0) -> true))
+  }
+
 }
--- src/test/scala/arraysandstrings/IsUniqueTest.scala
+++ src/test/scala/arraysandstrings/IsUniqueTest.scala
@@ -19,4 +19,19 @@ class IsUniqueTest extends FlatSpec with Matchers {
     IsUnique.isUnique("abca") should be(false)
   }
 
+  "isUniqueOptimized" should "return true if a String contains only unique characters"  in {
+    IsUnique.isUniqueOptimized("") should be(true)
+    IsUnique.isUniqueOptimized("a") should be(true)
+    IsUnique.isUniqueOptimized("ab") should be(true)
+    IsUnique.isUniqueOptimized("abc") should be(true)
+  }
+
+  it should "return false if a String contains repeated characters"  in {
+    IsUnique.isUniqueOptimized("aa") should be(false)
+    IsUnique.isUniqueOptimized("aba") should be(false)
+    IsUnique.isUniqueOptimized("aac") should be(false)
+    IsUnique.isUniqueOptimized("abcad") should be(false)
+    IsUnique.isUniqueOptimized("abca") should be(false)
+  }
+
 }
