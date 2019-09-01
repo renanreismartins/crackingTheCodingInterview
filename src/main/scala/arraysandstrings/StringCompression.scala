e6a594bb881ecebb06a465567d173fe4d230fab7
Renan Reis <renanreismartins@gmail.com>
2019-09-01 23:33:44 +0200

    StringCompresssion, final solution

--- src/main/scala/arraysandstrings/StringCompression.scala
+++ src/main/scala/arraysandstrings/StringCompression.scala
@@ -2,9 +2,22 @@ package arraysandstrings
 
 object StringCompression {
 
-  def compress(s: String, count: Int, acc: String): String = {
-    if (s.isEmpty)  acc
-    else ""
+  def compress(s: String): String = {
+    val compressed = compress_(s, 1, "")
+
+    if (compressed.size < s.size) compressed else s
+  }
+
+  def compress_(s: String, count: Int, acc: String): String = {
+    if (s.isEmpty) {
+      ""
+    } else if (s.size == 1) {
+      acc + s + count
+    } else if (s.charAt(0) == s.charAt(1)) {
+      compress_(s.drop(1), count + 1, acc)
+    } else {
+      compress_(s.drop(1), 1, acc + s.take(1) + count)
+    }
   }
 
 }
--- src/test/scala/arraysandstrings/StringCompressionTest.scala
+++ src/test/scala/arraysandstrings/StringCompressionTest.scala
@@ -5,16 +5,50 @@ import org.scalatest.{FlatSpec, Matchers}
 class StringCompressionTest extends FlatSpec with Matchers {
 
   it should "compress the string" in {
-    StringCompression.compress("aabcccccaaa", 0, "") should be("a2blc5a3")
+    StringCompression.compress("aabcccccaaa") should be("a2b1c5a3")
+  }
+
+  it should "return original string if the compressed is bigger" in {
+    StringCompression.compress("a") should be("a")
+    StringCompression.compress("ab") should be("ab")
+    StringCompression.compress("aab") should be("aab")
+    StringCompression.compress("abbcc") should be("abbcc")
+  }
+
+  it should "return original string if the compressed and original string are the same size" in {
+    StringCompression.compress("aa") should be("aa")
   }
 
   // not included in the book
   it should "return empty when the string is empty" in {
-    StringCompression.compress("", 0, "") should be("")
+    StringCompression.compress_("", 1, "") should be("")
   }
 
-  it should "return the original string when the compressed is bigger than the original" in {
-
+  it should "compress one character" in {
+    StringCompression.compress_("a", 1, "") should be("a1")
   }
 
+  it should "compress the same character when it appears twice" in {
+    StringCompression.compress_("aa", 1, "") should be("a2")
+  }
+
+  it should "compress the same character when it appears more than twice" in {
+    StringCompression.compress_("aaa", 1, "") should be("a3")
+  }
+
+  it should "compress two different characters" in {
+    StringCompression.compress_("ab", 1, "") should be("a1b1")
+  }
+
+  it should "compress string beginning with two identical characters and the last different" in {
+    StringCompression.compress_("aab", 1, "") should be("a2b1")
+  }
+
+  it should "compress string beginning with two different characters and last character identical to the previous" in {
+    StringCompression.compress_("abb", 1, "") should be("a1b2")
+  }
+
+  it should "compress single character followed by sequences of similar characters" in {
+    StringCompression.compress_("abbcc", 1, "") should be("a1b2c2")
+  }
 }
