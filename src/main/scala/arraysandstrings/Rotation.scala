9ec96b6ab1e84855f005920fdb997cf94047f1f3
Renan Reis <renanreismartins@gmail.com>
2019-09-28 15:53:17 +0200

    String Rotation; solution after the hints

--- src/main/scala/arraysandstrings/Rotation.scala
+++ src/main/scala/arraysandstrings/Rotation.scala
@@ -3,13 +3,7 @@ package arraysandstrings
 object Rotation {
 
   def isRotation(a: String, b: String): Boolean = {
-    val index = b.indexOf(a.charAt(0))
-    val splittedB = b.splitAt(index)
-
-    val r = splittedB._2.concat(splittedB._1)
-
-    a.equals(r)
-
+    a.concat(a).contains(b)
   }
 
 }
