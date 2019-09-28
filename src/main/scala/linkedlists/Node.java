6d0e5f0b68b98ff6e150ad3d64a1fb7b04dc44fa
Renan Reis <renanreismartins@gmail.com>
2019-09-28 18:30:28 +0200

    Remove duplicates in SingleLinked list, horrible code, but does the job. Also java given my lack of patience to translate into case classes and companion objects of scala

---
+++ src/main/scala/linkedlists/Node.java
@@ -0,0 +1,84 @@
+package linkedlists;
+
+
+import java.util.HashMap;
+import java.util.Objects;
+
+class Node {
+    public Node next = null;
+    int data;
+
+    public Node(int d) {
+        data = d;
+    }
+
+    public Node(int d, Node next) {
+        data = d;
+        this.next = next;
+    }
+
+    void appendToTail(int d) {
+        Node end = new Node(d);
+        Node n = this;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    public static Node deleteNode(Node head, int d) {
+        Node n = head;
+        if (n.data == d) {
+            return head.next;
+        }
+
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+                return head; /* head didn't change*/
+            }
+            n = n.next;
+        }
+        return head;
+    }
+
+    public static Node removeDuplicates(Node head) {
+        HashMap<Integer, Boolean> m = new HashMap<>();
+        m.put(head.data, true);
+
+        Node n = head;
+
+        while (n != null && n.next != null) {
+            if (m.getOrDefault(n.next.data, false)) {
+                n.next = n.next.next;
+            } else {
+                m.put(n.next.data, true);
+                n = n.next;
+            }
+        }
+
+        return head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return data == node.data &&
+                Objects.equals(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(next, data);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "next=" + next +
+                ", data=" + data +
+                '}';
+    }
+}
---
+++ src/test/scala/linkedlists/NodeTest.scala
@@ -0,0 +1,29 @@
+package linkedlists
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class NodeTest extends FlatSpec with Matchers {
+
+  it should "return the same list when there is only one element" in {
+    val n = new Node(1)
+    Node.removeDuplicates(n) should be(new Node(1))
+  }
+
+  it should "remove the when there is one duplicate" in {
+    val n = new Node(1, new Node(1))
+    Node.removeDuplicates(n) should be(new Node(1))
+  }
+
+  it should "remove the when there is more than duplicate" in {
+    val n = new Node(1, new Node(1, new Node(1)))
+    Node.removeDuplicates(n) should be(new Node(1))
+  }
+
+  it should "return the same list when there is no duplicates" in {
+    val n = new Node(1, new Node(2, new Node(3)))
+    Node.removeDuplicates(n) should be(new Node(1, new Node(2, new Node(3))))
+  }
+
+
+
+}
