e1bd846b418c2e1e8fb32c1dd29367845eca168f
Renan Reis <renanreismartins@gmail.com>
2020-02-29 17:21:51 +0100

    Implement three stacks using one array. Push

--- src/main/scala/stacksandqueues/ThreeStackOneArray.java
+++ src/main/scala/stacksandqueues/ThreeStackOneArray.java
@@ -1,19 +1,39 @@
 package stacksandqueues;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ThreeStackOneArray {
 
     private final int NUMBER_OF_STACKS = 3;
     private final int stackSize;
     private int[] values;
 
+    private Map<Integer, Integer> stackUtilization;
+
     public ThreeStackOneArray(int stackSize) {
         this.stackSize = stackSize;
         values = new int[stackSize * NUMBER_OF_STACKS];
+
+        stackUtilization = new HashMap<>();
+        stackUtilization.put(0, 0);
+        stackUtilization.put(1, 3);
+        stackUtilization.put(2, 6);
     }
 
     public void push(int stackNum, int value) throws FullStackException {
-        values[0] = 1;
+        Integer position = stackUtilization.get(stackNum);
 
+        if (position == null) {
+            throw new FullStackException();
+        }
+
+        values[position] = value;
+        stackUtilization.compute(stackNum, (key, oldValue) -> {
+            Integer newPosition = oldValue + 1;
+
+            return newPosition % stackSize == 0 ? null : newPosition;
+        });
     }
 
     public int pop(int stackNum) {
--- src/test/scala/stacksandqueues/ThreeStackOneArrayTest.scala
+++ src/test/scala/stacksandqueues/ThreeStackOneArrayTest.scala
@@ -4,25 +4,66 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ThreeStackOneArrayTest extends FlatSpec with Matchers {
 
-  it should "add element to stack one" in {
+  it should "add element to stacks" in {
     val stacks = new ThreeStackOneArray(3)
     stacks.push(0, 1)
-
-    stacks.getValues should be(Array[Int](1, 0, 0, 0, 0, 0, 0, 0, 0))
-  }
-
-  it should "add element to stack two" in {
-    val stacks = new ThreeStackOneArray(3)
     stacks.push(1, 1)
-
-    stacks.getValues should be(Array[Int](0, 0, 0, 1, 0, 0, 0, 0, 0))
-  }
-
-  it should "add element to stack three" in {
-    val stacks = new ThreeStackOneArray(3)
     stacks.push(2, 1)
 
-    stacks.getValues should be(Array[Int](0, 0, 0, 0, 0, 0, 1, 0, 0))
+    stacks.getValues should be(Array[Int](1, 0, 0, 1, 0, 0, 1, 0, 0))
   }
 
+  it should "add two elements to stacks" in {
+    val stacks = new ThreeStackOneArray(3)
+    stacks.push(0, 1)
+    stacks.push(0, 2)
+    stacks.push(1, 1)
+    stacks.push(1, 2)
+    stacks.push(2, 1)
+    stacks.push(2, 2)
+
+    stacks.getValues should be(Array[Int](1, 2, 0, 1, 2, 0, 1, 2, 0))
+  }
+
+  it should "fill the stacks" in {
+    val stacks = new ThreeStackOneArray(3)
+    stacks.push(0, 1)
+    stacks.push(0, 2)
+    stacks.push(0, 3)
+    stacks.push(1, 1)
+    stacks.push(1, 2)
+    stacks.push(1, 3)
+    stacks.push(2, 1)
+    stacks.push(2, 2)
+    stacks.push(2, 3)
+
+    stacks.getValues should be(Array[Int](1, 2, 3, 1, 2, 3, 1, 2, 3))
+  }
+
+  it should "add more elements than allowed the stacks" in {
+    val stacks = new ThreeStackOneArray(3)
+    stacks.push(0, 1)
+    stacks.push(0, 2)
+    stacks.push(0, 3)
+
+    stacks.push(1, 1)
+    stacks.push(1, 2)
+    stacks.push(1, 3)
+
+    stacks.push(2, 1)
+    stacks.push(2, 2)
+    stacks.push(2, 3)
+
+    assertThrows[FullStackException] {
+      stacks.push(0, 4)
+    }
+
+    assertThrows[FullStackException] {
+      stacks.push(1, 4)
+    }
+
+    assertThrows[FullStackException] {
+      stacks.push(2, 4)
+    }
+  }
 }
